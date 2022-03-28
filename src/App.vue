28b535b9d00de1fa57ec04f05383a0af79382430
Matt Palcic <matt.palcic@expedata.net>
2022-03-28 15:24:11 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+import {onMounted} from 'vue'
+import {IonApp, IonButton, IonItem, IonLabel, IonPage} from '@ionic/vue'
+import {Camera, CameraResultType, CameraSource} from '@capacitor/camera'
+import {defineCustomElements} from '@ionic/pwa-elements/loader'
+
+onMounted(async () => {
+  await defineCustomElements(window);
+})
+
+const snap = async () => {
+  const permissions = {
+    permissions: ["photos","camera"]
+  }
+
+  const options = {
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Prompt,
+    quality: 70,
+  }
+  await Camera.getPhoto(options)
+}
+
+</script>
+
+<template>
+  <ion-app>
+    <ion-page>
+      <ion-item>
+        <ion-label>Camera Issue</ion-label>
+        <ion-button @click="snap">Snap</ion-button>
+      </ion-item>
+    </ion-page>
+  </ion-app>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import {IonicVue} from '@ionic/vue'
+
+createApp(App)
+  .use(IonicVue)
+  .mount('#app')
